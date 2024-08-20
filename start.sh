5ed028c8106678dce15544b88ffdcbf5055f85dd
NplusM420 <Namassey05@gmail.com>
2024-08-20 02:17:37 -0500

    Create virtual environment in start.sh if it doesn't exist

--- start.sh
+++ start.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
-source venv/bin/activate  
-pip install psycopg2-binary 
-gunicorn backend.app:app 
+if [ ! -d "venv" ]; then  # Check if the 'venv' directory exists
+  python3 -m venv venv    # Create it if it doesn't exist
+fi
+source venv/bin/activate 
+pip install psycopg2-binary  
+gunicorn backend.app:app
